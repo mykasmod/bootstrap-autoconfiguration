e9b6d8e8c81b3b9ac5ca31e045536fb36743be31
mykasmod <mykasmod@gmail.com>
2018-06-16 15:39:42 +0800

    Add @Configuration Add @ConditionalOnClass Add @ConditionalOnProperty
    Add @PostConstruct

--- src/main/java/org/asmod/microservices/autoconfiguration/LogTomcatVersionAutoConfiguration.java
+++ src/main/java/org/asmod/microservices/autoconfiguration/LogTomcatVersionAutoConfiguration.java
@@ -1,12 +1,22 @@
-	package org.asmod.microservices.autoconfiguration;
+package org.asmod.microservices.autoconfiguration;
+
+import javax.annotation.PostConstruct;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.web.ServerProperties.Tomcat;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ConditionalOnClass(Tomcat.class)
+@ConditionalOnProperty(name = "log-tomcat-version", matchIfMissing = false)
 public class LogTomcatVersionAutoConfiguration {
 	private static Log logger = LogFactory.getLog(LogTomcatVersionAutoConfiguration.class);
 	
+	@PostConstruct
 	public void logTomcatVersion() {
-		logger.info("\n\n\nTomcat v");
+		logger.info("\n\n\nTomcat v" + Tomcat.class.getPackage().getImplementationVersion() + "\n\n");
 	}
 }
